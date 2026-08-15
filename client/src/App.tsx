75184b31f1e32e31823c53c1f4f18e5d928f5346
Lun4R-420 <adelynaspoiala@gmail.com>
2026-08-15 23:26:27 +0300

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,52 @@
 import Board from './components/Board';
 import { useBoardStore } from "./store/boardStore";
 import { useEffect } from "react";
+import { Routes, Route, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-function App() {
+function HomePage() {
+    const navigate = useNavigate();
     const createBoard = useBoardStore((state) => state.createBoard);
 
     useEffect(() => {
-        createBoard();
-    }, [createBoard]);
+        async function create() {
+            await createBoard();
+
+            const board = useBoardStore.getState().board;
+            if (board) {
+                navigate(`/boards/${board.id}`);
+            }
+        }
+
+        create();
+    }, [createBoard, navigate]);
+
+    return (
+        <p>Creating board...</p>
+    );
+}
+
+function BoardPage() {
+    const { boardId} = useParams();
+    const loadBoard = useBoardStore((state) => state.loadBoard);
+
+    useEffect(() => {
+        if (boardId) {
+            loadBoard(boardId);
+        }
+    }, [loadBoard, boardId]);
 
     return (
         <Board />
+    );
+}
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/boards/:boardId" element={<BoardPage />} />
+        </Routes>
     );
 }
 
